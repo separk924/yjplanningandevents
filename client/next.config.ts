db3a7a3d57b222dc6f7a475257073922ec3da049
Seung Park <separk924@gmail..com>
2025-10-23 19:09:02 -0700

    updated some files

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  appDir: true, // enable App Router (SSR) at the top level
-  // output: 'export', // keep commented to allow SSR
 };
 
 export default nextConfig;
